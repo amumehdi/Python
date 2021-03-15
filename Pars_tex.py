b4d9703730cf63da18a034dc542703b070e55de0
Mehdi Seraj Ansari <m.ansari@springa.tech>
2021-03-15 12:25:47 +0100

    the first plot is seen just coppy the file name and the variable you want to plot

--- Pars_tex.py
+++ Pars_tex.py
@@ -1,9 +1,48 @@
 import matplotlib.pyplot as plt
-f=open('..\sdddd.txt',"r")
-VarNames=["a","b","c","d","e","f","g"]
+import numpy as np
+import re
+FileName='sdddd.txt'
+Fields=["F1","F2","F3","F4","F5","F6","F7","F8","F9","F10","F11","F12",]
+F1=[]
+F2=[]
+F3=[]
+F4=[]
+F5=[]
+F6=[]
+F7=[]
+F8=[]
+F9=[]
+F10=[]
+F11=[]
+F12=[]
 
-Data=f.readlines()
-counter=0
-for i_list in Data:
-    counter=counter +1
-    V=(i_list.split(" ")).strip(" \n")
+f=open(FileName,"r")
+FirstLine=f.readline()
+FieldsNum=len(FirstLine.split(" "))
+DataLines=f.readlines()
+DataAll=[0]*len(DataLines)
+line_counter=0
+VarOut=[0]*FieldsNum
+for i_list in DataLines:
+    i_list=i_list.strip('\nn')               # but it is useless ! it doens't strip
+    line_in_string=i_list.split(" ")
+    line_in_float=[0]*FieldsNum
+    float_counter=0
+    for number_string in line_in_string:
+        line_in_float[float_counter]=float(number_string)
+        str2eval=Fields[float_counter]+'.append(float(number_string))'
+        eval(str2eval) 
+        float_counter=float_counter+1
+    DataAll[line_counter]=line_in_float
+    line_counter=line_counter +1
+
+
+
+print(DataAll[2][4])
+print(DataAll[1][9])
+X=np.arange(0,len(F1),1)
+
+fig, ax = plt.subplots()  # Create a figure and an axes.
+plt.plot(np.arange(0,len(F1),1),F1)
+plt.show()
+eval('print(DataAll[1][9])')
