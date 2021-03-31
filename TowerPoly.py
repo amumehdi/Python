fc8b7acfdff32f4c064ddd4319e50de00a616563
Mehdi Seraj Ansari <m.ansari@springa.tech>
2021-03-31 19:16:01 +0200

    it can plot

--- TowerPoly.py
+++ TowerPoly.py
@@ -1,29 +1,28 @@
 import matplotlib.pyplot as plt
 
 
-int(ENC_MAX)=26240
+ENC_MAX=26240
 
 Enc=range(1,ENC_MAX)
 down_coef=[ 2.74668e-17, -4.71125e-11, 0.00593861, 68.3226]
 up_coef=[3.81871e-17, -5.75944e-11, 0.00594275, 68.1941]
 
-DownPoly=[0]
-UpPoly=[0]
+DownPoly=[]
+UpPoly=[]
 downval=0
 upval=0
 for i in Enc:
-    downval= i*i*i*down_coef[1]+i*i*down_coef[2]+i*down_coef[3]+down_coef[4]
+    downval= i*i*i*down_coef[0]+i*i*down_coef[1]+i*down_coef[2]+down_coef[3]
     DownPoly.append(downval)
-    upval=i*i*i*up_coef[1]+i*i*up_coef[2]+i*up_coef[3]+up_coef[4]
-    UpPoly.appen(upval)
+    upval=i*i*i*up_coef[0]+i*i*up_coef[1]+i*up_coef[2]+up_coef[3]
+    UpPoly.append(upval)
 
 
 fig=plt.figure()
-plt.plot(ENC,UpPoly,label="IncPoly")
-plt.plot(ENC,DownPoly,label="DecPoly")
+plt.plot(Enc[:],UpPoly,label="IncPoly")
+plt.plot(Enc[:],DownPoly,label="DecPoly")
 plt.legend()
 plt.grid()
-plt.ion()
 plt.ylabel('mm') 
 plt.xlabel('encoder tick')
 
