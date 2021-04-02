59fbacd007dcc1ea92ffcc93513f3177d48fd426
Mehdi Seraj Ansari <m.ansari@springa.tech>
2021-04-02 19:00:48 +0200

    tower analyssis added

--- Pars_tex.py
+++ Pars_tex.py
@@ -1,9 +1,9 @@
+# %%
 import matplotlib.pyplot as plt
 import numpy as np
 import re
 from tkinter import Tk     # from tkinter import Tk for Python 3.x
 from tkinter.filedialog import askopenfilename
-
 
 Tk().withdraw() # we don't want a full GUI, so keep the root window from appearing
 Filename = askopenfilename() # show an "Open" dialog box and return the path to the selected file
@@ -31,7 +31,7 @@ DataAll=[0]*len(DataLines)
 line_counter=0
 VarOut=[0]*FieldsNum
 for i_list in DataLines:
-    i_list=i_list.strip('\nn')               # but it is useless ! it doens't strip
+    i_list=i_list.strip('\n')               # but it is useless ! it doens't strip
     line_in_string=i_list.split(" ")
     line_in_float=[0]*FieldsNum
     float_counter=0
@@ -43,27 +43,62 @@ for i_list in DataLines:
     DataAll[line_counter]=line_in_float
     line_counter=line_counter +1
 
+# %%
 
+def add_int_decimal(x_f,x_decimal):
+    return float(int(x_f)+x_decimal/10000)
 
-print(DataAll[2][4])
-print(DataAll[1][9])
-X=np.arange(0,len(F1),1)
+error_x_map= map(lambda xg, xr: xg-xr, F1, F5)
+error_y_map= map(lambda yg, yr: yg-yr, F2, F6)
+error_x=list(error_x_map)
+error_y=list(error_y_map)
+x_precise= list(map(add_int_decimal, F1, F11 ))
+y_precise= list(map(add_int_decimal, F2, F12 ))
 
-fig, ax = plt.subplots()  # Create a figure and an axes.
-plt.plot(np.arange(0,len(F1),1),F1,label="Goliath Position x")
-plt.plot(np.arange(0,len(F2),1),F2,label="Goliath Position y")
-#plt.plot(np.arange(0,len(F3),1),F3,label="Goliath Position z")
-#plt.plot(np.arange(0,len(F4),1),F4,label="Goliath Position Theta")
-#plt.plot(np.arange(0,len(F5),1),F5,label="Ref Position x")
-#plt.plot(np.arange(0,len(F6),1),F6,label="Ref Position y")
+# %%
+####### these should always remain the same
+fig1=plt.figure(1) # Create a figure and an axes.
+#plt.plot(np.arange(0,len(F1),1),F1,label="tcp Position x")
+#plt.plot(np.arange(0,len(F2),1),F2,label="tcp Position y")
+plt.plot(np.arange(0,len(x_precise),1),x_precise,label="tcp x precise")
+plt.plot(np.arange(0,len(y_precise),1),y_precise,label="tcp y precise")
+
+#plt.plot(np.arange(0,len(F3),1),F3,label="tcp Position z")
+#plt.plot(np.arange(0,len(F4),1),F4,label="tcp Position Theta")
+plt.plot(np.arange(0,len(F5),1),F5,label="Ref Position x")
+plt.plot(np.arange(0,len(F6),1),F6,label="Ref Position y")
 #plt.plot(np.arange(0,len(F7),1),F7,label="Ref Position z")
-#plt.plot(np.arange(0,len(F8),1),F8,label="Ref Position Theta")
-plt.plot(np.arange(0,len(F9),1),F9,label="Odo Filt X")
-plt.plot(np.arange(0,len(F10),1),F10,label="Odo Filt Y")
-plt.plot(np.arange(0,len(F11),1),F11,label="Sensor Filt X")
-plt.plot(np.arange(0,len(F12),1),F12,label="Sensor Filt y")
+#plt.plot(np.arange(0,len(F8),1),F8,label="Ref Position theta")
 plt.legend()
+plt.grid()
+#plt.ioff()
 plt.ylabel('Values') 
 plt.xlabel('Samlpes')
+
+# %%
+fig2=plt.figure(2)
+plt.plot(F1,F2,label="Goliath Trajectory")
+plt.plot(F5,F6,label="Generated Refrence")
+plt.legend()
+plt.grid()
+plt.ylabel('Y') 
+plt.xlabel('X')
+# %%
+fig3=plt.figure(3)
+plt.plot(np.arange(0,len(error_x)), error_x, label="error x ")
+plt.plot(np.arange(0,len(error_y)), error_y, label="error y ")
+plt.legend()
+plt.grid()
+plt.ylabel('Y') 
+plt.xlabel('X')
+# %%
+#######
+
+#plt.plot(np.arange(0,len(F9),1),F9,label="goliathPos.theta")
+#plt.plot(np.arange(0,len(F10),1),F10,label="theta_odo_accu_filt")
+#plt.plot(np.arange(0,len(F11),1),F11,label="x decimal ")
+#plt.plot(np.arange(0,len(F12),1),F12,label="y decimal")
 plt.show()
-eval('print(DataAll[1][9])')
+plt.close()
+
+
--- TowerPoly.py
+++ TowerPoly.py
@@ -1,5 +1,5 @@
 import matplotlib.pyplot as plt
-
+import numpy as np
 
 ENC_MAX=26240
 
@@ -9,18 +9,28 @@ up_coef=[3.81871e-17, -5.75944e-11, 0.00594275, 68.1941]
 
 DownPoly=[]
 UpPoly=[]
+errorPoly=[]
 downval=0
 upval=0
+error=0
 for i in Enc:
     downval= i*i*i*down_coef[0]+i*i*down_coef[1]+i*down_coef[2]+down_coef[3]
+    #downval=np.polyval(down_coef,i)
     DownPoly.append(downval)
     upval=i*i*i*up_coef[0]+i*i*up_coef[1]+i*up_coef[2]+up_coef[3]
     UpPoly.append(upval)
+    error=upval-downval
+    errorPoly.append(error)
+# %%
+error_coef=np.polyfit(Enc[:],errorPoly,1)
+errorpoly1d=np.poly1d(error_coef)
+errorp=np.linspace(1,ENC_MAX,ENC_MAX)
 
-
-fig=plt.figure()
+fig=plt.figure(1)
 plt.plot(Enc[:],UpPoly,label="IncPoly")
 plt.plot(Enc[:],DownPoly,label="DecPoly")
+plt.plot(Enc[:],errorPoly,label="error")
+plt.plot(errorp,errorpoly1d(errorp),label="errorpoly")
 plt.legend()
 plt.grid()
 plt.ylabel('mm') 
